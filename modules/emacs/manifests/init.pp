a84649e2c504d84368f9033c3bd84540a4600c5e
JP Akibako <jp@akibako>
2014-05-08 17:23:52 -0600

    Still working on emacs.

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -1,6 +1,10 @@
 class emacs {
+  file { '/tmp/emacs_243.deb':
+    source => 'puppet:///modules/emacs/myemacs_243.deb',
+  }
   package { "myemacs_243":
     provider => "dpkg",
-    source => 'puppet:///modules/emacs/myemacs_243.deb',
+    source => '/tmp/emacs_243.deb',
+    before => File['/tmp/emacs_243.deb'],
   }
 }
