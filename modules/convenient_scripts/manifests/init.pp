c70c0c5cd1230c4c0ed5d70e8ba4bc30a66c9514
JP Akibako <jp@akibako>
2014-05-08 12:01:32 -0600

    Forgot script location in manifest. Fixed.

--- modules/convenient_scripts/manifests/init.pp
+++ modules/convenient_scripts/manifests/init.pp
@@ -1,6 +1,6 @@
 class convenient_scripts {
   file { '/usr/local/bin/papply':
-    source  => 'puppet:///modules/puppet/papply.sh',
+    source  => 'puppet:///modules/convenient_scripts/papply.sh',
     mode    => '0755',
   }
 }
