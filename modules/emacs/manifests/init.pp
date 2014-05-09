a8bdb74e8f1f53b3928152c0bf7697d85e0b79c7
JP Akibako <jp@akibako>
2014-05-09 05:48:47 -0600

    Come on.

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -2,9 +2,9 @@ class emacs {
   file { '/tmp/emacs_243.deb':
     source => 'puppet:///modules/emacs/myemacs_243.deb',
   }
-  package { "myemacs_243":
-    provider => "dpkg",
-    source => '/tmp/emacs_243.deb',
-    before => File['/tmp/emacs_243.deb'],
-  }
+#  package { "myemacs_243":
+#    provider => "dpkg",
+#    source => '/tmp/emacs_243.deb',
+#    before => File['/tmp/emacs_243.deb'],
+#  }
 }
