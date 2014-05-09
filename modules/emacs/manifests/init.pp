104223519e6403f2a106d18bdb6b0ec94d20b462
JP Akibako <jp@akibako>
2014-05-09 06:01:03 -0600

    .

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -2,9 +2,8 @@ class emacs {
   file { '/tmp/emacs_243.deb':
     source => 'puppet:///modules/emacs/myemacs_243.deb',
   }
-#  package { "myemacs_243":
-#    provider => "dpkg",
-#    source => '/tmp/emacs_243.deb',
-#    before => File['/tmp/emacs_243.deb'],
-#  }
+  package { "myemacs_243":
+    source => '/tmp/emacs_243.deb',
+    before => File['/tmp/emacs_243.deb'],
+  }
 }
